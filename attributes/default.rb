03729966b55dad1c97deb2e3325e19212536ad86
Logan Koester <logan@logankoester.com>
2014-09-26 03:01:09 +0000

    ossec.url attribute is required for correctly fetching version 2.8.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['ossec']['checksum'] = '0ecf1df09558dc8bb4b6f65e1fb2ca7a7df9817c'
 default['ossec']['version'] = '2.8.1'
+default['ossec']['url'] = 'http://www.ossec.net/files/ossec-hids-2.8.1.tar.gz'
 default['ossec']['syscheck_freq'] = 14400 # Every 4 hours
 default['ossec']['disable_config_generation'] = true
 default['ossec']['logs'] = []
