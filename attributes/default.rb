a7a638b4e3196f5f46468b96fa7e13db29fe807b
Logan Koester <logan@logankoester.com>
2014-09-26 06:28:15 +0000

    Enable archlinux.journald.forward_to_syslog by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
+default['archlinux']['journald']['forward_to_syslog'] = 'yes'
+
 default['ossec']['checksum'] = '0ecf1df09558dc8bb4b6f65e1fb2ca7a7df9817c'
 default['ossec']['version'] = '2.8.1'
 default['ossec']['url'] = 'http://www.ossec.net/files/ossec-hids-2.8.1.tar.gz'
