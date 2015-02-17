977514856c4d0e0adb12a86911313910e27b49e4
Logan Koester <logan@logankoester.com>
2015-02-17 20:36:45 +0000

    Chef 12 seems to have upgraded this hash check from sha1 to sha256

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['archlinux']['journald']['forward_to_syslog'] = 'yes'
 
-default['ossec']['checksum'] = '13fa3fa61ab1e75a3a8adc6672de960e1edd21a4'
+default['ossec']['checksum'] = 'f1d8859c926db379530193579fb307ec4773561d68bbe5b698c19155f7965877'
 default['ossec']['version'] = '2.9.0-beta03'
 default['ossec']['url'] = 'https://github.com/ossec/ossec-hids/archive/2.9.0-beta03.tar.gz'
 default['ossec']['syscheck_freq'] = 14400 # Every 4 hours
