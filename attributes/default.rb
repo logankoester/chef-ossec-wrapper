2aca90e06d019de0f664d0bc7a9c52b875a0b152
Logan Koester <logan@logankoester.com>
2015-02-17 20:30:50 +0000

    Upgrade ossec-hids to 2.9.0-beta03

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['archlinux']['journald']['forward_to_syslog'] = 'yes'
 
-default['ossec']['checksum'] = '0ecf1df09558dc8bb4b6f65e1fb2ca7a7df9817c'
-default['ossec']['version'] = '2.8.1'
-default['ossec']['url'] = 'http://www.ossec.net/files/ossec-hids-2.8.1.tar.gz'
+default['ossec']['checksum'] = '13fa3fa61ab1e75a3a8adc6672de960e1edd21a4'
+default['ossec']['version'] = '2.9.0-beta03'
+default['ossec']['url'] = 'https://github.com/ossec/ossec-hids/archive/2.9.0-beta03.tar.gz'
 default['ossec']['syscheck_freq'] = 14400 # Every 4 hours
 default['ossec']['disable_config_generation'] = true
 default['ossec']['logs'] = []
