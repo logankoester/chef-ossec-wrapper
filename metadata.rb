28b8068252c38e1e3a7b1165a90133aa240a2103
Logan Koester <logan@logankoester.com>
2015-02-17 20:37:43 +0000

    Bumps 0.7.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'Wrapper for the OSSEC cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.0'
+version          '0.7.1'
 supports 'arch'
 depends 'syslog-ng'
 depends 'ossec'
